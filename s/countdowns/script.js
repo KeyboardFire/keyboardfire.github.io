15e4d974494ab41cd4c2322055de7fd7fbbea40f
KeyboardFire <andy@keyboardfire.com>
2017-11-11 11:01:59 -0600

    lots more countdowns

--- s/countdowns/script.js
+++ s/countdowns/script.js
@@ -1,9 +1,17 @@
 var data = [
-    { id: 'school', name: 'school starts', happens: 1503464400 },
-    { id: 'czech', name: 'Czech Duolingo', happens: 1504242000 },
-    { id: 'korean', name: 'Korean Duolingo', happens: 1504760400 },
-    { id: 'open', name: 'NACLO open round', happens: 1516860000 },
-    { id: 'inv', name: 'NACLO invitational round', happens: 1520488800 }
+    { id: 'school', name: 'school starts',            happens: 1503464400 },
+    { id: 'czech',  name: 'Czech Duolingo',           happens: 1504242000 },
+    { id: 'korean', name: 'Korean Duolingo',          happens: 1504760400 },
+    { id: 'gci',    name: 'GCI',                      happens: 1511848800 },
+    { id: 'usaco1', name: 'USACO 1',                  happens: 1513576800 },
+    { id: 'wint',   name: 'winter break',             happens: 1513836000 },
+    { id: 'usaco2', name: 'USACO 2',                  happens: 1516341600 },
+    { id: 'open',   name: 'NACLO open round',         happens: 1516860000 },
+    { id: 'amc',    name: 'AMC',                      happens: 1517983200 },
+    { id: 'usaco3', name: 'USACO 3',                  happens: 1519365600 },
+    { id: 'aime',   name: 'AIME',                     happens: 1520316000 },
+    { id: 'inv',    name: 'NACLO invitational round', happens: 1520488800 },
+    { id: 'usacoo', name: 'USACO open',               happens: 1521781200 },
 ];
 
 window.addEventListener('load', function() {
